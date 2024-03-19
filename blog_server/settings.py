1c2ebadc401f843625c04c2adce78a5bfd190568
孤舟江海寄余生 <319874759@qq.com>
2024-03-19 21:24:17 +0800

    fix(db): migration

--- blog_server/settings.py
+++ blog_server/settings.py
@@ -96,14 +96,10 @@ WSGI_APPLICATION = "blog_server.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'test_db.sqlite3',
-    },
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    },
+    }
 }
 
 
